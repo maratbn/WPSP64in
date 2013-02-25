7377b5809a26e1d91d01d51cb803ea40f7727e0f
Marat Nepomnyashy <maratbn@gmail.com>
2013-02-24 17:33:53 -0800

    Added license header to 'widget.php'.

--- widget.php
+++ widget.php
@@ -1,4 +1,37 @@
 <?php
+    /**
+     *  WPSP@in (WPSP64in) -- WordPress plugin for SP@in (SP64in) website component
+     *                        for CAPTCHA-protecting email addresses from email-
+     *                        address-harvesting web crawlers
+     *
+     *  Version: 0.0.1.next
+     *
+     *  Copyright (C) 2013  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
+     *
+     *  http://maratbn.com/projects/sp64in
+     *
+     *  Module:         widget.php
+     *
+     *  Description:    This is the logic for the sidebar widget.
+     *
+     *  Licensed under the GNU General Public License Version 3.
+     *
+     *  This file is part of WPSP@in (WPSP64in).
+     *
+     *  WPSP@in (WPSP64in) is free software: you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation, either version 3 of the License, or
+     *  (at your option) any later version.
+     *
+     *  WPSP@in (WPSP64in) is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  You should have received a copy of the GNU General Public License
+     *  along with WPSP@in (WPSP64in).  If not, see <http://www.gnu.org/licenses/>.
+     */
+
     namespace plugin_WPSP64in;
 
     class EmailWidget extends \WP_Widget {
